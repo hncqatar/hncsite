1f73228ba41809ba0996248da7ace4f7f9ed3a10
hncqatar <hncdigital@gmail.com>
2026-05-25 20:11:28 +0300

    fix gitignore and remove node_modules from gthg

--- server/server.js
+++ server/server.js
@@ -7,11 +7,11 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173" || "http://business.hotncool.qa";
 
 app.use(
   cors({
-    origin: [FRONTEND_URL, "http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: [FRONTEND_URL, "http://localhost:5173", "http://business.hotncool.qa", "http://127.0.0.1:5173"],
     methods: ["POST"],
   })
 );
